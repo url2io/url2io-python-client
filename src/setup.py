65c5dc1b775d949f393c3bfaabf691a175ba5486
leilux <leilux.dev@gmail.com>
2020-12-05 01:21:22 +0800

    add markdown readme and license to pypi

--- src/setup.py
+++ src/setup.py
@@ -30,12 +30,12 @@ REQUIRES = [
     "urllib3>=1.23"
 ]
 
-# # The directory containing this file
-# HERE = os.path.abspath(os.path.dirname(__file__))
-#
-# # The text of the README file
-# with open(os.path.join(HERE, "README.md")) as fid:
-#     README = fid.read()
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "PyPi-README.md")) as fid:
+    README = fid.read()
     
 setup(
     name=NAME,
@@ -47,9 +47,7 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    URL2io API SDK包含 URL2Article 和 URL2NLP 两个服务，实现网页结构智能解析和文本信息智能处理。
-    SDK 使用文档，见 https://github.com/url2io/url2io-python-client 。
-    SDK 涉及的 Rest API，见 http://url2io.applinzi.com/docs
-    """
+    license="MIT",
+    long_description_content_type='text/markdown',
+    long_description=README,
 )
